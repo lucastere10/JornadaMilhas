a495431940cd1ea379596b502f8b36d98961518a
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-05-13 11:46:40 -0300

    feat: Testes unitarios para OfertaViagem

---
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -0,0 +1,48 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemConstrutor
+    {
+        [Theory]
+        [InlineData("", null, "2024-01-01", "2024-01-02", 0, false)]
+        [InlineData("OrigemTeste", "Destino Teste", "2024-01-01", "2024-01-02", 100, true)]
+        [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+        [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+        [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+        public void RetornaValidoDeAcordoComDadosDeEntrada(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
+        {
+            Rota rota = new Rota(origem, destino);
+            Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao, oferta.EhValido);
+        }
+
+        [Fact]
+        public void RetornaMensagemDeErroDeRotaOuPeriodoInvalidosQuandoRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), DateTime.Now);
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
+
+        [Fact]
+        public void RetornaMensagemDeErroDePrecoInvalidoQuandoPrecoMenorQueZero()
+        {
+            Rota rota = new Rota("OrigemTeste", "Destino Teste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), DateTime.Now);
+            double preco = -100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
+    }
+}
